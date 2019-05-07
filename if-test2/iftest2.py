e90798520b29291e1e75ba364edfbfec47f01d8f
Mona Lisa <work33333work33333@outlook.com>
2019-05-07 11:38:40 -0400

    paramiko 1

--- if-test2/iftest2.py
+++ if-test2/iftest2.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
-ipchk = '192.168.0.1'
+#ipchk = 10.1.1.0
+ipchk = input('Apply in IP address: ')
 
 if ipchk:
     print('Looks like the IP address was set: ' + ipchk)
+else: #not
+    print('Error no input')
+
+
 
--- paramikosshrsa/learn2.py
+++ paramikosshrsa/learn2.py
@@ -7,10 +7,6 @@ def commandissue(command_to_issue):
     ssh_stdin, ssh_stdout, ssh_stderr = sshsession.exec_command(command_to_issue)
     return ssh_stdout.read()
 
-#def getcommand():
-#    our_commands = [input('Please input command with quotes: ')] 
-#    return our_commands
-
 sshsession = paramiko.SSHClient()
 
 # x = commandissue('ls')
@@ -21,12 +17,8 @@ sshsession.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 sshsession.connect(hostname="10.10.2.3", username="bender", pkey=mykey)
 
-
-our_commands = [input('Please input command string in quotes: ')]
-#our_commands = ['touch sshworked.txt', 'touch file3.txt', 'touch create.txt', 'ls']
+our_commands = ['touch sshworked.txt', 'touch file3.txt', 'touch create.txt', 'ls']
 #our_commands = our_commands.get()
-
-#getcommand()
 
 for x in our_commands:
    # print(commandissue(x))
