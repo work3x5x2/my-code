1c8ef98c590ac58581880a28be9d22b985220cc6
Mona Lisa <work33333work33333@outlook.com>
2019-05-10 11:03:47 -0400

    right before lunch

---
+++ script/pyhello.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+print("Hello, again my friend!  How are you?")
+myfile = open("/home/student/my-code/script/stopper.txt", 'a')
+myfile.write(" test append")
+myfile.close()
